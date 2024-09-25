a01fbde72111a541df40a307da24d34d8075a9a2
KESAVI WEB SOLUTIONS <106302838+kesaviwebsolutions@users.noreply.github.com>
2024-09-25 06:54:07 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Sancho - The good times!",
   description:
-    "Most of us degens are too caught up hunting for the 'the next banger' to handle our women's needs, whether it’s emotional or... you know, the other stuff. That’s where Sancho comes in — perfect for the typical degen who’s always grinding and just doesnt have the time to put in the work",
+    "Behind every successful man, there is a Sancho.",
   openGraph: {
     images: [
       "https://res.cloudinary.com/dmlbfqbox/image/upload/v1726997092/akpo8vzt9o67tv0tnwhd.jpg",
